bb5dc2cf6686e9a80ff4a53431c6bfb44d22f279
Ron Cohen <ronc.ntear@gmail.com>
2011-04-06 15:43:57 +0300

    Initial commit. Drupal 6 with contributed modules.

---
+++ sites/all/contemplates/node-blog-rss.tpl.php
@@ -0,0 +1,2 @@
+<?php print $node->content['body']['#value'] ?>
+<?php print $node->links['node_read_more']['href'] ?>
---
+++ sites/all/contemplates/node-deed-rss.tpl.php
@@ -0,0 +1,5 @@
+<div class="field field-type-date field-field-when">
+  <?php print $node->field_when[0]['view'];?>
+</div>
+<?php print $node->content['body']['#value'] ?>
+<?php print $node->links['node_read_more']['href'] ?>
