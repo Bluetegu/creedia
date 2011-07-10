c1d777dfb3d2b97fc9c27a30a7a4f66698aab07d
Ron Cohen <ronc.ntear@gmail.com>
2011-07-10 09:22:33 +0300

    Add h2 attribute to support collapsiblock in drupal 6.

--- sites/all/themes/ctendu/block-twitter.tpl.php
+++ sites/all/themes/ctendu/block-twitter.tpl.php
@@ -1,6 +1,6 @@
 <div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block_type ?>">
 <?php if ($block->subject): ?>
-<div class="collapse-handle"></div><h2><?php print $block->subject; ?></h2>
+<div class="collapse-handle"><h2></h2></div><h2><?php print $block->subject; ?></h2>
 <?php endif;?>
 <div class="content"><?php print $block->content ?></div>
 </div>
