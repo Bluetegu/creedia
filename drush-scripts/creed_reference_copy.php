87546a7bc19d8b26b106c4d325483b2518cc7af0
Ron Cohen <ronc.ntear@gmail.com>
2011-09-18 18:10:12 +0000

    fix creed reference copy drush script.

--- drush-scripts/creed_reference_copy.php
+++ drush-scripts/creed_reference_copy.php
@@ -4,7 +4,7 @@
 
 $result = db_query("SELECT * FROM {content_field_creed_reference1}");
 
-while($data = $db_result($result)) {
+while($data = db_fetch_object($result)) {
   $sql = "INSERT INTO {content_field_creed_reference} (vid, delta, nid, field_creed_reference_nid) VALUES (%d, %d, %d, %d)";
   db_query($sql, $data->vid, $data->delta, $data->nid, $data->field_creed_reference1_nid);
 }
