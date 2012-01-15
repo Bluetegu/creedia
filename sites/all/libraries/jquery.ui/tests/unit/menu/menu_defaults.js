263c75c1a1635cfee691bbacd47aab78256ed9aa
Ron Cohen <ronc@swifto.com>
2012-01-15 07:51:38 +0200

    jquery.ui library.

---
+++ sites/all/libraries/jquery.ui/tests/unit/menu/menu_defaults.js
@@ -0,0 +1,14 @@
+commonWidgetTests( "menu", {
+	defaults: {
+		disabled: false,
+		position: {
+			my: "left top",
+			at: "right top"
+		},
+		menus: "ul",
+		trigger: null,
+
+		// callbacks
+		create: null
+	}
+});
